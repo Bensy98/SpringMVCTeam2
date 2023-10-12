099b1981008b787170660e56c4a73fc91ef68172
ajay@7753 <ajayks3105@gmail.com>
2023-10-12 12:07:17 +0530

    my third commit

--- GitDemo/src/com/demo/service/EmployeeInter.java
+++ GitDemo/src/com/demo/service/EmployeeInter.java
@@ -5,5 +5,10 @@ public interface EmployeeInter {
 	public void search();
 
 	public void hello();
+	
+	public void AddEmployeedetails();
+	public void SearchByEmployee();
+	public void DeleteByEmployee();
+	
 
 }
